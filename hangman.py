4b5bfc77de5f92fba1c323f995251d89be2eb40e
Igor Fernandes <fernandesigor09@gmail.com>
2018-12-29 17:07:34 -0200

    Futher refining the game with hit numbers, number of erros and even better checking letters in the list.

--- hangman.py
+++ hangman.py
@@ -2,29 +2,40 @@
 def play_hangman():
     print('Hangman game')
 
-    word_secret = 'Banana'
+    word_secret = 'Banana'.upper()
     successful_letters = ['_', '_', '_', '_', '_', '_']
 
     hanged = False # Checks the player hanged
     right = False  # Check players' correctness
+    error = 0
 
     print(successful_letters)
 
     while (not hanged and not right):
 
-        kick = str(input('What letter ? ')).strip()
+        kick = str(input('What letter ? ')).strip().upper()
         # kick = kick.strip()
 
-        index = 0
-        for letter in word_secret:
+        if (kick in word_secret):
 
-            if (kick.upper() == letter.upper()):
-                successful_letters[index] = letter
-            index += 1
+            index = 0
+            for letter in word_secret:
 
+                if (kick == letter):
+                    successful_letters[index] = letter
+                index += 1
+        
+        else:
+            error += 1
+
+        hanged = error == 6
+        right = '_' not in successful_letters
         print(successful_letters)
-        print('playing ...')
-    
+
+    if (right):
+        print('You win!!')
+    else:
+        print('You lose!! ')
     print('Game Over')
 
 
