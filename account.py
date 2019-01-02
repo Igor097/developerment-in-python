7acbe75eadeb44554983956871aa8f4bee089066
Igor Fernandes <fernandesigor09@gmail.com>
2019-01-02 19:47:29 -0200

    Implementation of private methods and attributes.

--- account.py
+++ account.py
@@ -15,25 +15,33 @@ class Account:
     def deposit(self, value):
         self.__balance += value
 
+    def __check_drawings(self, value):
+        available_value = self.__limit + self.__limit
+        return value <= available_value
+
     def drawings(self, value):
-        if (self.__balance > self.__limit):
-            print('Error')
-        else:
+        if (self.__check_drawings(value)):
             self.__balance -= value
+        else:
+            print('Value {} has exceeded the limit'.format(value))
 
     def tranfer(self, value, destiny):
         self.deposit()
         destiny.drawings(value)
+    
+    @property
+    def holder(self, holder):
+        return self.__holder
 
-    def set_balance(self, balance):
-        self.__balance = balance
-
-    def get_balance(self):
+    @property
+    def balance(self):
         return self.__balance
 
-    def set_limit(self, limit):
+    @limit.setter
+    def limit(self, limit):
         self.__limit = limit
 
+    @property
     def get_limit(self):
         return self.__limit
 
